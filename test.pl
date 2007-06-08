96d27e668e450c91d8af4422be0976e4c9a96b7a
kingsebi (Sebastian Kaiser) <sebastian.kaiser@stat.uni-muenchen.de>
2007-06-08 12:26:33 +0000

    Neue Tags in Perl file codiert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spock@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- test.pl
+++ test.pl
@@ -8,11 +8,11 @@
    my $importance = 1;
    my $number = 0;
    sub text {
-       my ($self, $text) = @_;
+       my ($self, $text, $tag) = @_;
        
        while ( $text =~ /([A-Z][a-zA-Z]+)[\s\.,]/g ) {
         $number++;
-        print $1 . ';' . $importance . ';' . $number . "\n";
+        print $1 . ';' . $tag . ';' . $importance . ';' . $number . "\n";
        };
 
    }
@@ -21,8 +21,32 @@
 {
 my($self, $tag, $attr, $attrseq, $origtext) = @_;
 # $attr is reference to a HASH, $attrseq is reference
-			if ( $tag eq "b" ) {
+	if ( $tag eq "b" ) {
         $importance = 2
+        }
+        if ( $tag eq "k" ) {
+        $importance = 2
+        }
+        if ( $tag eq "h1" ) {
+        $importance = 3
+        }
+	if ( $tag eq "h2" ) {
+        $importance = 4
+        }
+        if ( $tag eq "h3" ) {
+        $importance = 5
+        }
+	if ( $tag eq "h4" ) {
+        $importance = 6
+        }
+        if ( $tag eq "h5" ) {
+        $importance = 7
+        }
+	if ( $tag eq "h6" ) {
+        $importance = 8
+        }
+        if ( $tag eq "u" ) {
+        $importance = 9
         }
 }
 
